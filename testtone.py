54889ff77aef2db4fb8cf8eda2feb2f54af143d0
Max-Mills <max33134@gmail.com>
2018-09-18 12:21:38 -0400

    created test file

---
+++ testtone.py
@@ -0,0 +1,20 @@
+def string2bits(s=''):
+    return [bin(ord(x))[2:].zfill(8) for x in s]
+
+def bits2string(b=None):
+    return ''.join([chr(int(x, 2)) for x in b])
+
+s = input("input command")
+b = string2bits(s)
+s2 = bits2string(b)
+
+print ('String:')
+print (s)
+
+print ('\nList of Bits:')
+for x in b:
+    print (x)
+
+print ('\nString:')
+print (s2)
+

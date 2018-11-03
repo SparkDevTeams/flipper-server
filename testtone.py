b2931827ab4fa05c6cf4dad2e2fee13d09193ec5
rslay <rhern283@fiu.edu>
2018-11-03 15:33:19 -0400

    Reformatted for hex

--- testtone.py
+++ testtone.py
@@ -11,40 +11,59 @@ BIT_INTERVAL_TIME = 0.15
 BETWEEN_BIT_DELAY = 0.12
 BETWEEN_BYTE_WAIT_TIME = 0.05
 
+FQ_0 = 17800
+FQ_1 = 17950
+FQ_2 = 18100
+FQ_3 = 18250
+FQ_4 = 18400
+FQ_5 = 18550
+FQ_6 = 18700
+FQ_7 = 18850
+FQ_8 = 19000
+FQ_9 = 19150
+FQ_A = 19300
+FQ_B = 19450
+FQ_C = 19600
+FQ_D = 19750
+FQ_E = 19900
+FQ_F = 20050
+
 print("DEFAULT PLAYBACK DEVICE:")
 try:
-	default_speaker = sc.default_speaker()
-	print(default_speaker)
-	pass
+    default_speaker = sc.default_speaker()
+    print(default_speaker)
+    pass
 except Exception:
-	print("Unable to find default playback devices.")
-	pass
+    print("Unable to find default playback devices.")
+    pass
 
 print()  # Prints an empty line
 
 print("DEFAULT RECORDING DEVICE:")
 try:
-	default_mic = sc.default_microphone()
-	print(default_mic)
-	pass
+    default_mic = sc.default_microphone()
+    print(default_mic)
+    pass
 except Exception:
-	print("Unable to find default recording device.")
-	pass
+    print("Unable to find default recording device.")
+    pass
 
 
 def string2bits(s=''):
-	"""Function to convert string to binary"""
-	return [bin(ord(x))[2:].zfill(8) for x in s]
+    """Function to convert string to binary"""
+    return [bin(ord(x))[2:].zfill(8) for x in s]
 
 
 def string2hex(s=''):
-	"""Function to convert string to binary"""
-	return [hex(x)[2:].zfill(8) for x in s]
+    """Function to convert string to binary"""
+    result = [hex(ord(x))[2:] for x in s]
+    return result
+    # return [hex(x)[2:].zfill(8) for x in s]
 
 
 def bits2string(b=None):
-	"""Function to convert binary to string"""
-	return ''.join([chr(int(x, 2)) for x in b])
+    """Function to convert binary to string"""
+    return ''.join([chr(int(x, 2)) for x in b])
 
 
 print("Client ready.")
@@ -52,40 +71,61 @@ print("Client ready.")
 default_speaker = sc.all_speakers()[1]
 while True:
 
-	# Input allows us to prompt user for command input
-	s = input("Input command \n>")
-	# This takes the users input and translates it to binary
-	b = string2bits(s)
+    # Input allows us to prompt user for command input
+    s = input("Input command \n>")
+    # This takes the users input and translates it to binary
+    b = string2hex(s)
 
-	s2 = bits2string(b)
+    # s2 = bits2string(b)
 
-	# Start timer
-	t0 = time.time()
-	# os.system(s2)
-	i = 0
-	# while i is less than the number of items in the array of bytes,
-	# 	go through every byte's individual bits and play them as high and low tones
-	while i < len(b):
-		# for every individual binary
-		for x in b[i]:
-			# emit high tone
-			if x == "0":
-				flipper.sine_tone(19000, BIT_INTERVAL_TIME)
-			# emit low tone
-			elif x == "1":
-				flipper.sine_tone(20050, BIT_INTERVAL_TIME)
-			else:
-				print("error")
-			time.sleep(BETWEEN_BIT_DELAY)
+    # Start timer
+    t0 = time.time()
+    # os.system(s2)
+    i = 0
+    # while i is less than the number of items in the array of bytes,
+    # go through every byte's individual bits and play them as high and low tones
+    for i in b:
+        for x in i:
+            if x == "0":
+                flipper.sine_tone(FQ_0, BIT_INTERVAL_TIME)
+            elif x == "1":
+                flipper.sine_tone(FQ_1, BIT_INTERVAL_TIME)
+            elif x == "2":
+                flipper.sine_tone(FQ_2, BIT_INTERVAL_TIME)
+            elif x == "3":
+                flipper.sine_tone(FQ_3, BIT_INTERVAL_TIME)
+            elif x == "4":
+                flipper.sine_tone(FQ_4, BIT_INTERVAL_TIME)
+            elif x == "5":
+                flipper.sine_tone(FQ_5, BIT_INTERVAL_TIME)
+            elif x == "6":
+                flipper.sine_tone(FQ_6, BIT_INTERVAL_TIME)
+            elif x == "7":
+                flipper.sine_tone(FQ_7, BIT_INTERVAL_TIME)
+            elif x == "8":
+                flipper.sine_tone(FQ_8, BIT_INTERVAL_TIME)
+            elif x == "9":
+                flipper.sine_tone(FQ_9, BIT_INTERVAL_TIME)
+            elif x == "a":
+                flipper.sine_tone(FQ_A, BIT_INTERVAL_TIME)
+            elif x == "b":
+                flipper.sine_tone(FQ_B, BIT_INTERVAL_TIME)
+            elif x == "c":
+                flipper.sine_tone(FQ_C, BIT_INTERVAL_TIME)
+            elif x == "d":
+                flipper.sine_tone(FQ_D, BIT_INTERVAL_TIME)
+            elif x == "e":
+                flipper.sine_tone(FQ_E, BIT_INTERVAL_TIME)
+            elif x == "f":
+                flipper.sine_tone(FQ_F, BIT_INTERVAL_TIME)
+            else:
+                print("error")
+            time.sleep(BETWEEN_BIT_DELAY)
 
+        # time.sleep(BETWEEN_BYTE_WAIT_TIME)
+    print(b)
 
-		# time.sleep(BETWEEN_BYTE_WAIT_TIME)
-		i += 1
-
-	# print the bits for verification
-	print(b)
-
-	# End timer
-	t1 = time.time()
-	total = t1-t0
-	print("(Time taken: ", total, " seconds)")
+    # End timer
+    t1 = time.time()
+    total = t1-t0
+    print("(Time taken: ", total, " seconds)")
